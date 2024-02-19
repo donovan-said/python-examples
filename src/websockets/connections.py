4d7c3d76af6c8f258d307e70d0ddc9a29206d8be
Donovan Said <donovan.s.said@gmail.com>
2024-02-19 11:01:03 +0000

    Initial commit.

---
+++ src/websockets/connections.py
@@ -0,0 +1,22 @@
+""" Webscoket Connections
+"""
+
+from websocket import create_connection
+
+
+def websocket_connection():
+    """A fuinction to create a Websocket connection"""
+
+    ws = create_connection("ws://echo.websocket.events/")
+    print(ws.recv())
+    print("Sending test message...")
+    ws.send("TEST MSG")
+    result = ws.recv()
+    print("Recieved '%s" % result)
+    print("Closing connection...")
+    ws.close()
+    print("Connection closed.")
+
+
+if __name__ == "__main__":
+    websocket_connection()
